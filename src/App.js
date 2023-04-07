5f448b9d230e3c72a47a992bbfe07d15b63a1a62
Viet Tran <viettran101294@gmail.com>
2023-04-07 01:43:13 -0400

    Add home page template, not found template, and project template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import AddClientModal from "./components/AddClientModal";
-import Clients from "./components/Clients";
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-import Projects from "./components/Projects";
+import Home from "./pages/Home";
+import Project from "./pages/Project";
+import NotFound from "./pages/NotFound";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -32,12 +33,16 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModal />
-          <Projects />
-          <Clients />
-        </div>
+        <BrowserRouter>
+          <Header />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/projects/:id' element={<Project />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
       </ApolloProvider>
     </>
   );
