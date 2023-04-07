49357d23414fd971f31e6efdf3c06668d5cec611
Viet Tran <viettran101294@gmail.com>
2023-04-07 00:43:08 -0400

    Add header and spinner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import Header from "./components/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  uri: 'http://localhost:808/graphql',
+  cache,
+});
 
 function App() {
   return (
-    <div className="container">
-      Hello world
-    </div>
+    <>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          Hello world
+        </div>
+      </ApolloProvider>
+    </>
   );
 }
 
---
+++ src/components/Clients.jsx
@@ -0,0 +1,7 @@
+export default function Clients() {
+  return (
+    <>
+      <h2 className="text-center mb-4">Clients</h2>
+    </>
+  );
+}
